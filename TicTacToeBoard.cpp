e12810bea39784618c5656f378758877924e96dc
jgutierrezCSU <jgutierrez@mail.csuchico.edu>
2019-11-23 11:04:02 -0800

    Update TicTacToeBoard.cpp
    -fixed test for get winner

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -117,14 +117,13 @@ Piece TicTacToeBoard::getWinner()
   }
   //Blank if the board is filled and no one has won.
  for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 3; j++){
-			
-			if(board[i][j] == Blank)
-			{
-				return Blank;
-			}
-		}
-	}	
+    for(int j = 0; j < 3; j++){
+	if(board[i][j] != Blank)
+	{
+	return Blank;
+	}
+    }
+ }	
 	
 	return Invalid;
 
